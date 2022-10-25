c5030b9b8a8fbbc1dc9cb1a03b681575c21a293d
Falkreon <falkreon@gmail.com>
2022-10-24 19:03:44 -0500

    Manually fix LicenseFormat damage

--- src/main/java/com/playsawdust/glow/model/Material.java
+++ src/main/java/com/playsawdust/glow/model/Material.java
@@ -6,12 +6,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */
-
 package com.playsawdust.glow.model;
 
 public class Material extends AbstractShaderAttributeHolder {
